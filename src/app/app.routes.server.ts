3f3928f2fed497a98ba3527acfe161484e959e1e
LucasG04 <mail4lg@yahoo.de>
2025-08-08 15:23:55 +0200

    fixes server route rendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'feed',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'history',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'saved',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Server,
   },
 ];
