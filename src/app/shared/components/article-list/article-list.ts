b03c3f5b9df0005d315e1535b0901405248c9b10
LucasG04 <mail4lg@yahoo.de>
2025-08-08 12:37:24 +0200

    updates platform check and near scroll bottom detection

--- src/app/shared/components/article-list/article-list.ts
+++ src/app/shared/components/article-list/article-list.ts
@@ -4,6 +4,7 @@ import {
   inject,
   input,
   OnInit,
+  PLATFORM_ID,
   signal,
 } from '@angular/core';
 import { ArticleCard } from '../article-card/article-card';
@@ -12,6 +13,7 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { fromEvent, filter, firstValueFrom } from 'rxjs';
 import { ArticleService } from '../../../core/services/article-service';
 import { Article } from '../../types/article';
+import { isPlatformBrowser } from '@angular/common';
 
 @UntilDestroy()
 @Component({
@@ -21,6 +23,7 @@ import { Article } from '../../types/article';
   styleUrl: './article-list.css',
 })
 export class ArticleList implements OnInit, AfterViewInit {
+  private readonly platformId = inject(PLATFORM_ID);
   private readonly articleService = inject(ArticleService);
 
   getArticles =
@@ -32,37 +35,35 @@ export class ArticleList implements OnInit, AfterViewInit {
   ngOnInit(): void {
     this.getArticles()(0, 20)
       .then((articles) => this.articles.set(articles))
-      .catch((error) => console.error('Error loading feed:', error))
+      .catch((error) => console.error('Error loading articles:', error))
       .finally(() => this.isLoading.set(false));
   }
 
   ngAfterViewInit(): void {
-    // window not defined because of SSR platform has to be checked
-    if (typeof window === 'undefined') return;
-    fromEvent(window, 'scroll')
-      .pipe(
-        untilDestroyed(this),
-        filter(() => this.articles().length > 0) // Ensure articles are loaded before checking scroll
-      )
-      .subscribe(async () => {
-        const scrollPosition = window.scrollY + window.innerHeight;
-        const documentHeight = document.documentElement.scrollHeight;
+    if (isPlatformBrowser(this.platformId)) {
+      fromEvent(window, 'scroll')
+        .pipe(
+          untilDestroyed(this),
+          filter(() => this.articles().length > 0) // Ensure articles are loaded before checking scroll
+        )
+        .subscribe(async () => {
+          const scrollPosition = window.scrollY + window.innerHeight;
+          const documentHeight = document.documentElement.scrollHeight;
+          const scrollThreshold = window.innerHeight * 0.2;
 
-        if (scrollPosition >= documentHeight - 100 && !this.isLoading()) {
-          console.log(
-            'Load',
-            this.articles().length,
-            this.articles().length + 20
-          );
-          this.isLoading.set(true);
-          const next = await this.getArticles()(
-            this.articles().length,
-            this.articles().length + 20
-          );
-          this.articles.set([...this.articles(), ...next]);
-          this.isLoading.set(false);
-        }
-      });
+          const isNearBottom =
+            scrollPosition >= documentHeight - scrollThreshold;
+          if (isNearBottom && !this.isLoading()) {
+            this.isLoading.set(true);
+            const next = await this.getArticles()(
+              this.articles().length,
+              this.articles().length + 20
+            );
+            this.articles.set([...this.articles(), ...next]);
+            this.isLoading.set(false);
+          }
+        });
+    }
   }
 
   updateArticleSaved(articleId: string, save: boolean): void {
