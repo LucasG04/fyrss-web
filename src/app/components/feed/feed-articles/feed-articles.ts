225c93af12984e05243848a867f8df4e5e85f54e
LucasG04 <mail4lg@yahoo.de>
2025-10-12 18:43:44 +0300

    adds loading state and error handling for feed articles

--- src/app/components/feed/feed-articles/feed-articles.ts
+++ src/app/components/feed/feed-articles/feed-articles.ts
@@ -7,10 +7,11 @@ import { RssFeedService } from '../../../core/services/rss-feed-service';
 import { RssFeed } from '../../../shared/types/rss-feed';
 import { ArticleList } from '../../../shared/components/article-list/article-list';
 import { Location } from '@angular/common';
+import { Loader } from '../../../shared/components/loader/loader';
 
 @Component({
   selector: 'app-feed-articles',
-  imports: [ArticleList],
+  imports: [ArticleList, Loader],
   templateUrl: './feed-articles.html',
   styleUrl: './feed-articles.css',
 })
@@ -21,6 +22,7 @@ export class FeedArticles {
   private readonly rssFeedService = inject(RssFeedService);
 
   rssFeed = signal<RssFeed | null>(null);
+  isLoading = signal(true);
 
   constructor() {
     const feedId = this.route.snapshot.paramMap.get('id');
@@ -34,7 +36,8 @@ export class FeedArticles {
       .catch((err) => {
         console.error('Error loading feed:', err);
         this.rssFeed.set(null);
-      });
+      })
+      .finally(() => this.isLoading.set(false));
   }
 
   getFeed = async (from: number, to: number): Promise<Article[]> => {
